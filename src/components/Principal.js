f1903f5aba16e4709857fa480134f66e27e68547
Marcus Pontes <marcusporama@gmail.com>
2018-09-29 15:31:16 -0300

    Criado a página de resultado do jogo.

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -13,17 +13,28 @@ export default class Principal extends Component {
       <View style={styles.cenaprincipal}>
         <View style={styles.apresetacaoJogo}>
           <Image source={logo} />
+
+          <TouchableHighlight
+          underlayColor={'#61BD8C'}
+          activeOpacity={0.9}
+            onPress={() => { Actions.Resultado(); }}
+          >
           <Image source={btnJogar} />
+          </TouchableHighlight>
         </View>
 
           <View style={styles.rodape}>
             <TouchableHighlight
+            underlayColor={'#61BD8C'}
+            activeOpacity={0.9}
                 onPress={() => { Actions.Sobre(); }}
             >
                 <Image source={btnSobreJogo} />
             </TouchableHighlight>
 
             <TouchableHighlight
+            underlayColor={'#61BD8C'}
+            activeOpacity={0.9}
                  onPress={() => { Actions.OutrosJogos(); }}
             >
                 <Image source={btnOutrosJogos} />
---
+++ src/components/Resultado.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Image, View } from 'react-native';
+
+const cara = require('../imgs/moeda_cara.png');
+const coroa = require('../imgs/moeda_coroa.png');
+
+
+export default class Resultado extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.stat = { resultado: '' };
+
+    }
+
+    componentWillMount(){
+        const numAleatorio = Math.floor(Math.random() * 2); //mathfloor retira a parte inteira e math random faz a escolha do número de 0 a 1, * por 2 para ter número inteiro.
+        let caraOuCoroa = '';
+
+        if (numAleatorio === 0) {
+            caraOuCoroa = 'cara';
+        } else {
+            caraOuCoroa = 'coroa';
+        }
+    
+        this.setState({resultado: caraOuCoroa });
+    }
+
+    render() {
+        if (this.state.resultado === 'cara') {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#61BD8C', }}>
+                  <Image source={cara} />
+                  </View>
+            );
+        } else {
+            return (
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#61BD8C', }}>
+                 <Image source={coroa} />
+              </View>
+            );
+        }
+        
+    }
+  
+  }
