f42ac30f54c5bab10664f8e5a2f10805839000c9
Marcus Pontes <marcusporama@gmail.com>
2018-09-29 01:15:48 -0300

    implementado routerflux e criado principal.js

--- App.js
+++ App.js
@@ -1,43 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-
-const logo = require('./src/imgs/logo.png');
-const btnJogar = require('./src/imgs/botao_jogar.png');
-const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
-const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
+import { Router, Scene } from 'react-native-router-flux';
+import Principal from './src/components/Principal';
+import Sobre from './src/components/Sobre';
+import OutrosJogos from './src/components/OutrosJogos';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.cenaprincipal}>
-        <View style={styles.apresetacaoJogo}>
-          <Image source={logo} />
-          <Image source={btnJogar} />
-        </View>
-
-          <View style={styles.rodape}>
-            <Image source={btnSobreJogo} />
-            <Image source={btnOutrosJogos} />
-          </View>
-      </View>
+      <Router>
+        <Scene key='principal' component={Principal} />
+        <Scene key='sobre' component={Sobre} />
+        <Scene key='outros' component={OutrosJogos} />
+      </Router>
     );
   }
 
 }
-
-const styles = StyleSheet.create({
-  cenaprincipal: {
-    flex: 1,
-    backgroundColor: '#61BD8C'
-  },
-  apresetacaoJogo: {
-    flex: 10,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  rodape: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  }
-});
---
+++ src/components/Principal.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+const logo = require('../imgs/logo.png');
+const btnJogar = require('../imgs/botao_jogar.png');
+const btnSobreJogo = require('../imgs/sobre_jogo.png');
+const btnOutrosJogos = require('../imgs/outros_jogos.png');
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.cenaprincipal}>
+        <View style={styles.apresetacaoJogo}>
+          <Image source={logo} />
+          <Image source={btnJogar} />
+        </View>
+
+          <View style={styles.rodape}>
+            <Image source={btnSobreJogo} />
+            <Image source={btnOutrosJogos} />
+          </View>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  cenaprincipal: {
+    flex: 1,
+    backgroundColor: '#61BD8C'
+  },
+  apresetacaoJogo: {
+    flex: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rodape: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
