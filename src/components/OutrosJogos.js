99da2296e8542c29e57548011f98d998aa69cebd
Marcus Pontes <marcusporama@gmail.com>
2018-09-29 01:07:01 -0300

    Criação das duas páginas: sobre e outros jogos

---
+++ src/components/OutrosJogos.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class OutrosJogos extends Component {
+    render() {
+      return (
+        <Text style={{ flex: 1, backgroundColor: '61BD8C', fontWeight: 'bold', fontSize: 25, }}> Aqui fica os outros jogos do desenvolvedor. </Text>
+      );
+    }
+  
+  }
---
+++ src/components/Sobre.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class Sobre extends Component {
+    render() {
+      return (
+        <Text style={{ flex: 1, backgroundColor: '61BD8C', fontWeight: 'bold', fontSize: 25, }}> Aqui pode ser apresentado as informações do jogo </Text>
+      );
+    }
+  
+  }
