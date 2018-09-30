390ea7c6dbd944d37b020297abad616d91974059
Marcus Pontes <marcusporama@gmail.com>
2018-09-29 21:32:34 -0300

    Projeto Finalizado!

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Router, Scene } from 'react-native-router-flux';
 
-import Rotas from './src/Rotas';
+import Principal from './src/components/Principal';
+import OutrosJogos from './src/components/OutrosJogos';
+import Resultado from './src/components/Resultado';
+import Sobre from './src/components/Sobre';
 
-export default class App extends Component {
+export default class app extends Component {
   render() {
     return (
-      <Rotas />
+      <Router navigationBarStyle={{ backgroundColor: "#61BD8C" }}
+      titleStyle={{ color: "white" }}>
+        <Scene key='root'>
+          <Scene key='principal' component={Principal} initil title="Cara ou coroa" initial />
+          <Scene key='Sobre' component={Sobre} title="Sobre o Jogo" />
+          <Scene key='OutrosJogos' component={OutrosJogos} title="Outros Jogos" />
+          <Scene key='Resultado' component={Resultado} title="Resultado" />
+          </Scene>
+      </Router>
     );
   }
 }
